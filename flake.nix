f0d06219c98e01c2b53c03e48651953c60450f78
kalthun <jameskwm@protonmail.com>
2025-02-17 15:19:19 -0500

    updates

--- flake.nix
+++ flake.nix
@@ -1,4 +1,35 @@
 {
-  
+  description = "Python Dev-Shell";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+
+      supportedSystems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+      
+      forEachSupportedSystem = f: nixpkgs.lib.genAttrs supportedSystems (system: f { pkgs = import nixpkgs { inherit system; }; });
+
+    in {
+      devShells = forEachSupportedSystem ({ pkgs }: {
+        default = pkgs.mkShell {
+          packages = with pkgs; [
+            (python3.withPackages (ps: with ps; [
+              numpy
+              scipy
+              matplotlib
+              pandas
+              jupyter
+            ]))
+          ] ++ (if pkgs.system == "aarch64-darwin" then [ ] else [ gdb ]);
+        };
+      });
+    };
 }
-# TODO
